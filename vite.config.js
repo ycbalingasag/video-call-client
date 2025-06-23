858b8bba2d52676331cf3411b338d1bf80c2eaa7
ycbalingasag <ycbalingasag@gmail.com>
2025-06-24 03:16:41 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// ✅ vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+});
