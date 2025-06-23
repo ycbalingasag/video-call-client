31699e2d870cb9e0b5d8cb6ecf6a38c6753b792b
ycbalingasag <ycbalingasag@gmail.com>
2025-06-24 03:16:12 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
+// ✅ tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
