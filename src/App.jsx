cdc99b2102e105e76df0c6f8e0aedd0d84b936af
ycbalingasag <ycbalingasag@gmail.com>
2025-06-24 03:00:15 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,4 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Room from './pages/Room';
+// ✅ App.jsx (Router) import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; import Room from './pages/Room';
 
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/room/:roomId" element={<Room />} />
-      </Routes>
-    </Router>
-  );
-}
+export default function App() { return ( <Router> <Routes> <Route path="/room/:roomId" element={<Room />} /> </Routes> </Router> ); }
+
